571e2e38790859dcddb50eba5281857e534cce6b
bazilval <bazilval@bk.ru>
2022-09-15 19:08:40 +0300

    Started Embeddeds

--- spec/Model/Embedded/Embedded.cs
+++ spec/Model/Embedded/Embedded.cs
@@ -2,5 +2,23 @@
 {
     public class Embedded
     {
+        private IEmbeddedType type;
+        private Steel steel;
+        private int count;
+        public Element ParentElement { get; }
+        public IEmbeddedType Type { get => type; set { type = value; OnChange(); } }
+        public Steel Steel { get => steel; set { steel = value; OnChange(); } }
+        public int Count { get => count; set { count = value; OnChange(); } }
+        public double Mass { get => Type.Mass; }
+        public double TotalMass { get => Mass * Count; }
+        public string Mark { get; set; }
+        public string Description { get; set; }
+        void OnChange()
+        {
+            ParentElement.OnChange();
+        }
+        public bool IsReady() => (TotalMass != 0);
     }
+
+    
 }
---
+++ spec/Model/Embedded/EmbeddedType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace spec.Model
+{
+    public class EmbeddedType: IEmbeddedType
+    {
+    }
+    public interface IEmbeddedType
+    {
+    }
+}
